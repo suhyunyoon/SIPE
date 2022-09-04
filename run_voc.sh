54c12d89840c2c90c109019886d7ac8c55a870f5
suhyunyoon <billy555@naver.com>
2022-09-04 16:00:51 +0900

    Update voc script with session name

--- run_voc.sh
+++ run_voc.sh
@@ -1,19 +1,30 @@
+DATASET_ROOT=../../dataset/VOC/VOCdevkit/VOC2012
+SESSION="logs/sipe"
 
 # Step 1. Train SIPE for localization maps.
 
 # 1.1 train sipe
-python train_resnet50_SIPE.py
+python train_resnet50_SIPE.py --session_name ${SESSION} --dataset_root ${DATASET_ROOT}
+date
 # 1.2 obtain localization maps
-python make_cam.py
+python make_cam.py --session_name ${SESSION} --dataset_root ${DATASET_ROOT}
+date
 # 1.3 evaluate localization maps
-python eval_cam.py
-
+python eval_cam.py --session_name ${SESSION} --dataset_root ${DATASET_ROOT} --gt_path ${DATASET_ROOT}/SegmentationClass
+date
 
 # Step 2. Train IRN for pseudo labels.
 
 # 2.1 generate ir label
-python cam2ir.py
+python cam2ir.py --session_name ${SESSION} --dataset_root ${DATASET_ROOT}
+date
 # 2.2 train irn
-python train_irn.py
+python train_irn.py --session_name ${SESSION} --voc12_root ${DATASET_ROOT}
+date
 # 2.3 make pseudo labels
-python make_seg_labels.py
+python make_seg_labels.py --session_name ${SESSION} --voc12_root ${DATASET_ROOT}
+date
+
+# 2.4 eval
+python eval_sem_seg.py --sem_seg_out_dir ${SESSION}/pseudo_label --voc12_root ${DATASET_ROOT} --gt_dir ${DATASET_ROOT}/SegmentationClass
+date
